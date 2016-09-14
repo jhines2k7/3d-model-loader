8ad11cfedd3fb6bc011c0b5e26b079b4c5b2857f
James Hines <jameshines10@gmail.com>
2016-09-14 11:55:32 -0400

    Adds implementations for the init, update, and render methods of the ModelDemo driver class

--- src/com/hines/james/ModelDemo.java
+++ src/com/hines/james/ModelDemo.java
@@ -1,8 +1,15 @@
 package com.hines.james;
 
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+
 public class ModelDemo implements Runnable {
     private Thread thread;
     public boolean running = true;
+
+    private long window;
+
+    private int width = 1200, height = 800;
 
     public static void main(String[] args) {
         ModelDemo demo = new ModelDemo();
@@ -17,15 +24,29 @@ public class ModelDemo implements Runnable {
     }
 
     public void init(){
+        if(glfwInit() != true){
+            System.err.println("GLFW initialization failed!");
+        }
 
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+        window = glfwCreateWindow(width, height, "Teapot", 0L, 0L);
+
+        glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+        glfwSetWindowPos(window, 100, 100);
+
+        glfwMakeContextCurrent(window);
+
+        glfwShowWindow(window);
     }
 
     public void update(){
-
+        glfwPollEvents();
     }
 
     public void render(){
-
+        glfwSwapBuffers(window);
     }
 
     @Override
@@ -34,6 +55,10 @@ public class ModelDemo implements Runnable {
         while(running){
             update();
             render();
+
+            if(glfwWindowShouldClose(window) == true){
+                running = false;
+            }
         }
     }
 }
