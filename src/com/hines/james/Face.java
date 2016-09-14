25e1adaae8ef509688218990278be106c907644a
James Hines <jameshines10@gmail.com>
2016-09-14 10:10:07 -0400

    Renames the normals property of the Face class to normal. Adds a constructor to the Face class. Adds a list of Face objects to the Model calss

--- src/com/hines/james/Face.java
+++ src/com/hines/james/Face.java
@@ -4,5 +4,10 @@ import com.hines.james.math.Vector3f;
 
 public class Face {
     public Vector3f vertex = new Vector3f();
-    public Vector3f normals = new Vector3f();
+    public Vector3f normal = new Vector3f();
+
+    public Face(Vector3f vertex, Vector3f normal) {
+        this. vertex = vertex;
+        this.normal = normal;
+    }
 }
