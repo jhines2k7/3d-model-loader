09c35892321843ab6170ea50b9f9379b84f3f1d9
James Hines <jameshines10@gmail.com>
2016-09-14 10:05:52 -0400

    Turns out the version of LWJGL no longer includes any math utilities. I had to go find some...

---
+++ src/com/hines/james/Face.java
@@ -0,0 +1,8 @@
+package com.hines.james;
+
+import com.hines.james.math.Vector3f;
+
+public class Face {
+    public Vector3f vertex = new Vector3f();
+    public Vector3f normals = new Vector3f();
+}
---
+++ src/com/hines/james/Model.java
@@ -0,0 +1,14 @@
+package com.hines.james;
+
+import com.hines.james.math.Vector3f;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+    public List<Vector3f> vertices = new ArrayList<Vector3f>();
+    public List<Vector3f> normals = new ArrayList<Vector3f>();
+
+
+
+}
